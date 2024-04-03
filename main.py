10b56ae2b7addfbc40f33ffc7bc89fbfce1a14cd
Marco Junior <tombombadil182@gmail.com>
2024-04-02 23:30:36 -0300

    Starting to implement functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from karaoke_creator import KaraokeCreator
 
 def main():
-    youtube_link = "https://www.youtube.com/watch?v=DHEOF_rcND8"
-    karaoke_creator = KaraokeCreator(youtube_link)
+    youtube_url = "https://www.youtube.com/watch?v=DHEOF_rcND8"
+    karaoke_creator = KaraokeCreator(youtube_url)
     karaoke_creator.criar_karaoke()
 
 if __name__ == "__main__":
